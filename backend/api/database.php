5294f087267cb9dfe738c9d3c9a1b692ebd98faf
Rio0628 <mariomdomenech@gmail.com>
2022-02-01 15:24:45 -0500

    Fixed header from database.php

--- backend/api/database.php
+++ backend/api/database.php
@@ -3,7 +3,7 @@
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Methods: PUT, GET, DELETE, POST");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    header("Content-Type: application/json; charser=UTF-8");
+    header("Content-Type: application/json; charset=UTF-8");
 
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
