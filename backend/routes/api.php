12b2518ccd9fd44c55c956e8eb3f3970316e9770
Rio0628 <mariomdomenech@gmail.com>
2022-02-03 15:28:14 -0500

    Added Note Route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\NoteController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('note')->group(function () {
+    Router::get('/', [ NoteController::class, 'getAll']);
+    Router::post('/', [ NoteController::class, 'create']);
+    Router::delete('/{id}', [ NoteController::class, 'delete']);
+    Router::get('/{id}', [ NoteController::class, 'get']);
+    Router::put('/{id}', [ NoteController::class, 'update']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
