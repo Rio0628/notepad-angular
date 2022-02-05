7abc86a8e56cd79da494891c9ca06c83ef5b8f5f
Rio0628 <mariomdomenech@gmail.com>
2022-02-04 21:10:10 -0500

    Added Basic Add Note Functionality

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { Note } from '../../note';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,6 +12,7 @@ import { Note } from '../../note';
 export class DashboardComponent implements OnInit {
 
   addNoteToView = false;
+  form: FormGroup | any;
   notes: Note[] | undefined;
   
   constructor(private apiService: ApiService, private router: Router) { }
@@ -24,7 +26,20 @@ export class DashboardComponent implements OnInit {
     this.apiService.getAll().subscribe((notes: Note[]) => {
       this.notes = notes;
       console.log(this.notes);
-    })
+    });
+
+    this.form = new FormGroup({
+      name:  new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+      note_body:  new FormControl('<p>Enter Note Here</p>', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+    });
+  }
+
+  createNote() {
+    console.log(this.form.value);
+    this.apiService.create(this.form.value).subscribe(res => {
+      console.log('Person Created Successfully!');
+      // this.router.navigate(['/note']);
+    });
   }
 
   deleteNote(id: any) {
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { Note } from '../../note';
+import { FormGroup, FormControl, Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'app-sidebar',
@@ -12,6 +13,7 @@ export class SidebarComponent implements OnInit {
 
   @Input() sidebarOpen = '';
   addNoteToView = false;
+  form: FormGroup | any;
   notes: Note[] | undefined;
 
   constructor(private apiService: ApiService, private router: Router) {}
@@ -28,6 +30,19 @@ export class SidebarComponent implements OnInit {
     this.apiService.getAll().subscribe((notes: Note[]) => {
       this.notes = notes;
       console.log(this.notes);
+    })
+
+    this.form = new FormGroup({
+      name:  new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+      note_body:  new FormControl('<p>Enter Note Here</p>', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+    });
+  }
+
+  createNote() {
+    console.log(this.form.value);
+    this.apiService.create(this.form.value).subscribe(res => {
+      console.log('Person Created Successfully!');
+      // this.router.navigate(['/note']);
     })
   }
 
