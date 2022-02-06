f7643a87446b3fb35c0ca01fe3e3a290516a7173
Rio0628 <mariomdomenech@gmail.com>
2022-02-05 23:27:23 -0500

    Eliminated Searchbar Element and Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { OnInit, EventEmitter } from '@angular/core';
-import { SearchbarService } from './services/searchbar.service';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +11,10 @@ export class AppComponent implements OnInit {
   public title = 'notepad-angular';
   sidebarState = 'close';
 
-  constructor (private searchbarService: SearchbarService) { }
+  constructor () { }
 
   // notes = ApiService.readNotes();
-
-  searchbarFunc(e: any) {
-    this.searchbarService.updateSearchValue(e.target.value);
-    // this.searchValues = e.target.value;
-  }
-
+  
   setSidebarState = () => this.sidebarState === 'open' ? this.sidebarState = 'close' : this.sidebarState = 'open';
 
   ngOnInit() : void {
